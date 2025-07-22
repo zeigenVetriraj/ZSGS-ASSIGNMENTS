037d0e075edf30c6f275f0f7a4415393593a2384
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-22 15:34:39 +0530

    Update Pattern2.java

--- 1.Assignments for java recap/Day-2/Part-1/Pattern2.java
+++ 1.Assignments for java recap/Day-2/Part-1/Pattern2.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 /*
 3. Write a program to do the following patterns using while loop?
-a)1    b) w
-  2 3     w w
-  4 5 6   w w w
-  7 8     w w
-  9       w
+a)
+      1    
+    2   3   
+  4   5   6
+    7   8  
+      9 
+b)
+w
+w w
+w w w
+w w
+w
 */
 // inside git
 
@@ -14,44 +21,25 @@ public class Pattern2 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of lines: ");
-        int num = sc.nextInt(); // total lines like 5
+        int num = sc.nextInt();
 
-        System.out.print("Enter '1' for character pattern or '0' for number pattern: ");
-        int choice = sc.nextInt();
         sc.nextLine(); // consume newline
 
-        if (choice == 1) {
-            System.out.print("Enter the character: ");
-            char ch = sc.nextLine().charAt(0);
-            
-            int i = 1;
-            while (i <= num) {
-                int j = 1;
-                int limit = (i <= (num / 2 + 1)) ? i : (num - i + 1);
-				
-                while (j <= limit) {
-                    System.out.print(ch + " ");
-                    j++;
-                }
-                System.out.println();
-                i++;
-            }
+		System.out.print("Enter the character: ");
+		char ch = sc.nextLine().charAt(0);
+		
+		int i = 1;
+		while (i <= num) {
+			int j = 1;
+			int limit = (i <= (num / 2 + 1)) ? i : (num - i + 1);
+			
+			while (j <= limit) {
+				System.out.print(ch + " ");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
 
-        } else {
-            int val = 1;
-            int i = 1;
-            while (i <= num) {
-                int j = 1;
-                int limit = (i <= (num / 2 + 1)) ? i : (num - i + 1);
-				
-                while (j <= limit) {
-                    System.out.print(val + " ");
-                    val++;
-                    j++;
-                }
-                System.out.println();
-                i++;
-            }
-        }
     }
 }
