6aa866ef661862984e4927196791b4b41166466f
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-22 10:18:20 +0530

    Update Pattern1.java

--- 1.Assignments for java recap/Day-2/Part-1/Pattern1.java
+++ 1.Assignments for java recap/Day-2/Part-1/Pattern1.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 /*
 2. Write a program to do the following patterns using for loop?
-a) 1 1 R R R R
-1 1 R R
-1 R R R R
-1 1 R R
-1 1 R R
+a) 1       1  R R R R
+     1   1    R     R
+       1      R R R R
+     1   1    R   R
+   1       1  R     R
 */
 public class Pattern1 {
     public static void main(String[] args) {
@@ -14,6 +14,29 @@ public class Pattern1 {
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
 
-        
+        for(int i=0; i<num; i++){
+			for(int j=0; j<num; j++){
+				if(i==j || i+j==num-1)System.out.print("1 ");
+				else System.out.print("  ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=0,k=0; i<num; i++){
+			for(int j=0; j<num-1; j++){
+				if(num%2==0){
+					if(i==0 || i==num/2-1 || j==0 || (i<=num/2-1 && j==num-2) || (i>=num/2 && j==k))System.out.print("R ");
+					else System.out.print("  ");
+
+				}
+				else{
+					if(i==0 || i==num/2 || j==0 || (i<=num/2 && j==num-2)|| (i>=num/2-1 && j==k))System.out.print("R ");
+					else System.out.print("  ");
+				}
+			}
+			System.out.println();
+			if(num%2==0 && i>=num/2)k++;
+			else if(num%2==1 && i>=num/2-1)k++;
+		}
     }
 }
