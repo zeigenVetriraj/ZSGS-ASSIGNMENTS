0ea9f48c61253edea8f5a9370c1dd69ad7b5a23e
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-24 22:03:49 +0530

    Update Pattern1.java

--- 1.Assignments for java recap/Day-2/Part-1/Pattern1.java
+++ 1.Assignments for java recap/Day-2/Part-1/Pattern1.java
@@ -14,29 +14,31 @@ public class Pattern1 {
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
 
+	//for the X pattern
         for(int i=0; i<num; i++){
-			for(int j=0; j<num; j++){
-				if(i==j || i+j==num-1)System.out.print("1 ");
+		for(int j=0; j<num; j++){
+			if(i==j || i+j==num-1)System.out.print("1 ");
+			else System.out.print("  ");
+		}
+		System.out.println();
+	}
+
+	// Print R
+	for(int i=0,k=0; i<num; i++){
+		for(int j=0; j<num-1; j++){
+			if(num%2==0){
+				if(i==0 || i==num/2-1 || j==0 || (i<=num/2-1 && j==num-2) || (i>=num/2 && j==k))System.out.print("R ");
+				else System.out.print("  ");
+
+			}
+			else{
+				if(i==0 || i==num/2 || j==0 || (i<=num/2 && j==num-2)|| (i>=num/2-1 && j==k))System.out.print("R ");
 				else System.out.print("  ");
 			}
-			System.out.println();
 		}
-		
-		for(int i=0,k=0; i<num; i++){
-			for(int j=0; j<num-1; j++){
-				if(num%2==0){
-					if(i==0 || i==num/2-1 || j==0 || (i<=num/2-1 && j==num-2) || (i>=num/2 && j==k))System.out.print("R ");
-					else System.out.print("  ");
-
-				}
-				else{
-					if(i==0 || i==num/2 || j==0 || (i<=num/2 && j==num-2)|| (i>=num/2-1 && j==k))System.out.print("R ");
-					else System.out.print("  ");
-				}
-			}
-			System.out.println();
-			if(num%2==0 && i>=num/2)k++;
-			else if(num%2==1 && i>=num/2-1)k++;
-		}
+		System.out.println();
+		if(num%2==0 && i>=num/2)k++;
+		else if(num%2==1 && i>=num/2-1)k++;
+	}
     }
 }
