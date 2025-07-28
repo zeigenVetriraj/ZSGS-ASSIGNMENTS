91088c82ef85f88ebe0d1d96262bc779f19487e0
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-28 10:20:11 +0530

    Update FindPrime.java

--- 1.Assignments for java recap/Day-2/Part-2/FindPrime.java
+++ 1.Assignments for java recap/Day-2/Part-2/FindPrime.java
@@ -25,10 +25,10 @@ public class FindPrime{
 	// for finding the value is prime or not
 	public static boolean isPrime(int num){
 		if(num<2)return false;
-		if(num<==2)return true;
+		if(num==2)return true;
 		if(num%2==0)return false;
 		
-		for(int i=3; i*i<=num; i++){
+		for(int i=3; i*i<=num; i+=2){
 			if(num%i==0)return false;
 		}
 		return true;
